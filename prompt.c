cd36c37ef7cf8a3e5652a4c36b2a028f67203a1b
Santeri Horttanainen <santeri.horttanainen@gmail.com>
2015-06-05 08:03:14 +0300

    Prompt now allows user to look commanline history and move around with arrow keys.

--- prompt.c
+++ prompt.c
@@ -1,7 +1,32 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-/* Declare a buffer for user input of size 2048 */
-static char input[2048];
+/* If we are compiling on Windows compile these functions */
+#ifdef _WIN32
+#include <string.h>
+
+static char buffer[2048];
+
+/* Fake readline function */
+char* readline (char* prompt){
+    fputs(prompt,stdout);
+    fgets(buffer,2048,stdin);
+    char* cpy = malloc(strlen(buffer)+1);
+    strcpy(cpy,buffer);
+    cpy[strlen(cpy)-1] = '\0';
+    return cpy;
+}
+
+/* Fake add_history function */
+void add_history(char* unused){
+
+}
+
+/* Otherwise include the editline headers */
+#else
+#include <editline/readline.h>
+#include <editline/history.h>
+#endif
 
 int main(int argc, const char *argv[])
 {
@@ -12,14 +37,17 @@ int main(int argc, const char *argv[])
     /* In a never ending loop */
     while(1){
 
-        /* Output our prompt */
-        fputs("Lispy> ",stdout);
+        /* Output our prompt and get input */
+        char* input = readline("Lispy> ");
 
-        /* Read a line of user input of maximum size 2048 */
-        fgets(input,2048,stdin);
+        /* Add input to history */
+        add_history(input);
         
         /* Echo input back to user */
-        printf("No you're a %s",input);
+        printf("No you're a %s\n",input);
+
+        /* Free retrieved input */
+        free(input);
     }
 
     return 0;
